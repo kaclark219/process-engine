ec2808f257f6e8cc633691aab7394d61bf87bc3f
Clark <katelyn.clark@emerson.com>
2026-07-17 13:58:24 -0500

    Design details/documentation + prep for adding the rules agent implementation.

--- internal/memory/memory.go
+++ internal/memory/memory.go
@@ -14,5 +14,29 @@ type Memory struct {
 }
 
 func New() *Memory {
-	return &Memory{ values: make(map[string]*entry) }
+	m := &Memory{
+		values: make(map[string]*entry),
+	}
+
+	// test values
+	m.values["process.Tank01.Temperature"] = &entry{
+		value: map[string]any{
+			"value":     45.0,
+			"timestamp": "2026-07-17T10:15:00Z",
+		},
+	}
+	m.values["process.Pump03.Flow"] = &entry{
+		value: map[string]any{
+			"value":     120.0,
+			"timestamp": "2026-07-17T10:15:00Z",
+		},
+	}
+	m.values["process.Tank02.Temperature"] = &entry{
+		value: map[string]any{
+			"value":     75.0,
+			"timestamp": "2026-07-17T10:15:00Z",
+		},
+	}
+
+	return m
 }
